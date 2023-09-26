51cd0f965627a263a9c6ad377c4f7f1bdd407f8c
JTH10 <jthenrie@yahoo.com>
2023-09-26 13:41:29 -0600

    Updated header color, Roto Pat URL & search.

--- Projects/fantasy.py
+++ Projects/fantasy.py
@@ -5,21 +5,21 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-week = "13"
+week = "3"
 pos = ["QB", "WR", "RB", "TE", "K", "DEF"]
 for position in pos:
     if position == "QB":
-        player_name = ["Joe Burrow", "Deshaun Watson"]
+        player_name = ["Justin Fields", "Deshaun Watson"]
     elif position == "WR":
-        player_name = ["Courtland Sutton", "Amari Cooper", "Keenan Allen", "Chris Godwin", ]
+        player_name = ["Stefon Diggs", "Brandon Aiyuk", "Mike Williams"]
     elif position == "RB":
-        player_name = ["Joe Mixon","Alvin Kamara", "Miles Sanders", "Kenneth Walker III", "Latavius Murray"]
+        player_name = ["Bijan Robinson","Breece Hall", "James Conner", "Kenneth Gainwell", "De'Von Achane", "Kareem Hunt"]
     elif position == "TE":
-        player_name = ["George Kittle"]
+        player_name = ["Dallas Goedert"]
     elif position == "K":
-        player_name = ["Tyler Bass"]
+        player_name = ["Harrison Butker"]
     elif position == "DEF":
-        player_name = ["Dallas Cowboys", "New England Patriots"]
+        player_name = ["Buffalo Bills", "Philadelphia Eagles"]
 
     url = f"https://fantasy.nfl.com/research/rankings?leagueId=0&position={position}&statSeason=2022&statType=weekStats&week={week}"
 
@@ -28,7 +28,7 @@ for position in pos:
 
 
     print()
-    print('\033[1m' + '\033[4m' + f"NFL.com {position} week {week} rank" + '\033[0m')
+    print('\033[1m' + '\033[4m'+ '\033[94m' + f"NFL.com {position} week {week} rank" + '\033[0m')
 
     #Create a dictionary of players and rank to sort before printing.
     NFL_com = dict()
@@ -46,7 +46,7 @@ for position in pos:
             player_rank = doc.find(class_=f"player-{player_id}").td.string
             NFL_com[name] = int(player_rank)
 
-        # If player is not found input rank as 1000. Update "1000" to "Not Ranked" later on when printing
+        # If player is not found input rank as 1000. Update "1000" to "Not Ranked" later on when printing.
         except (IndexError):
             NFL_com[name] = 1000
             continue
@@ -69,14 +69,14 @@ for position in pos:
     else:
         Roto_position = position   
 
-    url =f"https://www.nbcsportsedge.com/article/nfl-rankings/week-{week}-fantasy-football-rankings-{Roto_position}"
+    url =f"https://www.nbcsports.com/fantasy/football/news/week-{week}-fantasy-football-rankings-{Roto_position}"
 
     result = requests.get(url)
     doc = BeautifulSoup(result.content, "html.parser")
 
     print()
     print()
-    print('\033[1m' + '\033[4m' + f"Roto Pat {position} week {week} rank" + '\033[0m')
+    print('\033[1m' + '\033[4m' + '\033[94m' + f"Roto Pat {position} week {week} rank" + '\033[0m')
 
     #Create a dictionary of players and rank to sort before printing.
     Roto_pat = dict()
@@ -85,17 +85,14 @@ for position in pos:
     for name in player_name:
         try:
             #Find the tag with the players name in it.
-            search_name = doc.find_all("td", text=name)
-
-            #Find the parent tag with the weekly rank.
-            prnt = search_name[0].parent
+            search_name = doc.find("td", colspan="1", text=name).parent
 
             #Find the tag with the player rank and strip away the rank.
-            Roto_rank = prnt.find("strong").text
+            Roto_rank = search_name.find("b").text
             Roto_pat[name] = int(Roto_rank)
 
         # If player is not found input rank as 1000. Update "1000" to "Not Ranked" later on when printing
-        except (IndexError):
+        except (IndexError, AttributeError):
             Roto_pat[name] = 1000
             continue
 
